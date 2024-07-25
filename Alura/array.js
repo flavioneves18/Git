25c4b984aec6c6a790ef1a68665f9de3b26bd4ba
Flavio Candeias <flavioneves18@gmail.com>
2024-07-25 14:47:04 -0300

    atualização

--- Alura/array.js
+++ Alura/array.js
@@ -1,1 +1,9 @@
-let 
+let valor = 6
+let numeros = [1,2,3,4,5];
+//numeros.pop() retira o ultimo valor
+let lista1 = numeros.slice(0,3); //intervalo para trazer, mas tem que guardar em uma variável
+numeros.push(valor); // insere valor
+console.log(numeros);
+console.log(lista1);
+
+
