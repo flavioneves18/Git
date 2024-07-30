69b9e82d32f7d390b5a2eb56977430e36b8345cb
Flavio Candeias <flavioneves18@gmail.com>
2024-07-30 11:02:13 -0300

    atualização

--- Alura/For.js
+++ Alura/For.js
@@ -1,1 +1,10 @@
-TextDecoderStream
+const lista = [10,6.5,8,7.5]
+var soma = 0
+
+for (let indice = 0; indice< lista.length; indice++){
+       var soma = (lista[indice] + soma)
+           //console.log(`O Número ${lista[indice]} está no indice ${indice}`);
+}
+let media = (soma / lista.length)
+console.log(media);
+
