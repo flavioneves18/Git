b79247e54ef9e8e7a04a776d91c077901b8ebd48
Flavio Candeias <flavioneves18@gmail.com>
2024-07-31 10:13:36 -0300

    atualização

--- Alura/ForEach_media.js
+++ Alura/ForEach_media.js
@@ -1,13 +1,19 @@
+const alunos = ["Ana", "Marcos", "Maria", "Mauro"];
 const notas = [10, 6.5, 8, 7.5];
  var contador = 0
  var soma =0
 notas.forEach((item,indice) => {
     soma += item
     contador++ 
-    
-});
+    });
 let media = soma / contador
-console.log(media)  
+
+
+const reprovados = alunos.filter((nota,indice) => {
+    return notas[indice] < 7;
+  });
+  
+  console.log(reprovados);
 
 
 
