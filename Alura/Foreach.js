612796b86fec87e8080105bf36e732b5f0d991a5
Flavio Candeias <flavioneves18@gmail.com>
2024-07-31 10:13:51 -0300

    atualização

--- Alura/Foreach.js
+++ Alura/Foreach.js
@@ -1,8 +1,20 @@
-const notas = [10,6.5,8,7.5];
-let soma =0;
-notas.forEach(function(nota){
-    }
-)
+const usuarios = [
+    {nome: "Rodolfo", idade: 33, contato: "(71)12345-8452"},
+    {nome: "Adilson", idade: 16, contato: "(71)12345-8252"},
+    {nome: "Mizerê", idade: 32, contato: "(71)14345-8452"},
+]
+//retorna três parametros: Item, Index e Array
+var contador =0;
+var maiores = [];
+usuarios.forEach(function(item){
+    if (item.idade > 18){
+    contador++;
+    maiores.push(item.nome)
+    }});
+    console.log(`Existem ${contador} com mais de 18 anos que são: ${maiores}`)
 
-soma =+ notas.length
-console.log(soma);
+   
+ 
+
+    
+ 
