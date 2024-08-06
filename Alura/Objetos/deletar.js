2ea76c714d5ab7219ae16a13dabfe3f4613e747a
Flavio Candeias <flavioneves18@gmail.com>
2024-08-06 10:04:39 -0300

    atualização

---
+++ Alura/Objetos/deletar.js
@@ -0,0 +1,7 @@
+const objPersonagem = {nome: "Gandalf", classe: "mago", nivel: "20",
+aliado: {nome: "Saruman",classe: "mago"},status: "desaparecido"
+}
+
+delete objPersonagem.classe //deleta
+
+console.log(objPersonagem)
