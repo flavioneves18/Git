656e8cf523d601af098913e37fa9f8874c39ff3b
Flavio Candeias <flavioneves18@gmail.com>
2024-08-08 15:29:34 -0300

    atualização

--- Alura/Objetos/funcoes.js
+++ Alura/Objetos/funcoes.js
@@ -16,3 +16,26 @@ const cliente = {
   };
 
   cliente.efetuaPagamento(40);
+
+  const cliente2 = {
+    nome: "Joao",
+    idade: function(ida) {
+        if (ida < 18) {
+            this.idade = `Menor de Idade, ${this.nome} tem apenas ${ida} anos`;
+        } else {
+            this.idade = `Maior de Idade, ${this.nome} Já tem ${ida} anos`;
+        }
+    },
+    email: "joao@firma.com",
+    telefone: ["1155555550", "1144444440"],
+  };
+  
+  cliente.enderecos = 
+    {
+      rua: "R. Joseph Climber",
+      numero: 1337,
+      apartamento: true,
+      complemento: "ap 934",
+    };
+    cliente2.idade(20)
+    console.log(cliente2)
