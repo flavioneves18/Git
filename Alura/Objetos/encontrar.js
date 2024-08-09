3ec22e19fa40e3de76c240984888784653a4c82d
Flavio Candeias <flavioneves18@gmail.com>
2024-08-09 10:20:05 -0300

    atualização

---
+++ Alura/Objetos/encontrar.js
@@ -0,0 +1,14 @@
+const clientes = require("./clientes.json");
+
+function encontrar(lista, chave, valor) {
+  return lista.find((item) => item[chave].includes(valor));
+}
+
+const encontrado = encontrar(clientes, "nome", "Kirby");
+
+console.log(encontrado.nome);
+
+const encontrado2 = encontrar(clientes, "telefone", "1918820860");
+
+console.log(encontrado2.nome);
+
