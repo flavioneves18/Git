724efa83d7b55e55ee3a6cb1882ec700442472fa
Flavio Candeias <flavioneves18@gmail.com>
2024-09-18 17:58:47 -0300

    atualização

---
+++ Estacio/python/exerciciopessoa.py
@@ -0,0 +1,6 @@
+from pessoa import Pessoa
+pessoa_1 = Pessoa("", 0)
+pessoa_1.setNome("Flavio das Candeias")
+pessoa_1.setIdade(32)
+
+print(f'O nome da pessoa_1 é {pessoa_1.getNome()} e a idade é {pessoa_1.getIdade()}.')
--- Estacio/python/while.py
+++ Estacio/python/while.py
@@ -2,7 +2,7 @@ palavra = input('Digite o comando: ')
 count = 0
 palavras = []
 while palavra != "sair":
-    palavra = input('Digite o comando: ')
-    count += 1
-print (f'Programa encerrado, foram digitadas {count} palavras')
-
+        palavra = input('Digite o comando: ')
+        count += 1
+        palavras.append(palavra)
+print (f'Programa encerrado, foram digitadas {count} palavras, e as palavras foram: {palavras}')
